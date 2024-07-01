b7c5e27308d264b86562d4ec64334f286e4bc0ca
Bence Ditrich <ditrichb@gmail.com>
2024-07-01 20:37:01 +0200

    Add not found page to prerender config

--- vite.config.ts
+++ vite.config.ts
@@ -39,6 +39,7 @@ export default defineConfig(({ mode }) => ({
             '/about-us/',
             '/blog/',
             ...contentFileRoutes,
+            '/not-found/',
           ];
         },
         sitemap: {
