ee97fbde2894d5518b266d2c5ebe297033d89c75
Bence Ditrich <ditrichb@gmail.com>
2024-07-03 01:36:34 +0200

    Revert "Add more code for debugging"
    This reverts commit 2edd01dd4b6413651576ae18a07dee82117a639a.

--- src/app/components/blog/blog-content.component.ts
+++ src/app/components/blog/blog-content.component.ts
@@ -131,7 +131,6 @@ export default class BlogContentComponent {
     try {
       return JSON.parse(decodedElement) as T;
     } catch (error) {
-      console.log(placeholder);
       console.log(decodedElement);
       throw Error('Something went wrong');
     }
