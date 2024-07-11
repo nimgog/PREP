1651a202ecf9429df24265165dd455a9e7c13e02
Bence Ditrich <ditrichb@gmail.com>
2024-07-11 21:25:02 +0200

    Extract blog article list to its own component to get rid of need for redirection

--- src/app/pages/blog/pages/index.page.ts
+++ src/app/pages/blog/pages/index.page.ts
@@ -1,6 +1,14 @@
-import { RouteMeta } from '@analogjs/router';
+import { Component } from '@angular/core';
+import BlogArticleListComponent, {
+  sharedRouteMeta,
+} from 'src/app/components/blog/blog-article-list.component';
 
-export const routeMeta: RouteMeta = {
-  redirectTo: '/blog/pages/1',
-  pathMatch: 'full',
-};
+export const routeMeta = sharedRouteMeta;
+
+@Component({
+  selector: 'app-blog-article-list-fallback-page',
+  standalone: true,
+  imports: [BlogArticleListComponent],
+  template: `<app-blog-article-list></app-blog-article-list>`,
+})
+export default class BlogArticleListFallbackPageComponent {}
